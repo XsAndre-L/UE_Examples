78f0fbb166ec007567ecaca7490c528e562b053f
XsAndre-L <ahloubser12@gmail.com>
2022-11-20 18:31:56 +0200

    initial commit

---
+++ Source/UE_Examples/UE_ExamplesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_ExamplesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_EXAMPLES_API AUE_ExamplesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
